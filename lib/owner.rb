37999d332ce174816d36022c1adbe9934b7feced
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 15:47:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,29 @@ class Owner
   attr_accessor :name
   attr_reader :species
   
+@@all = []
+  
   def initialize(name)
     @name = name
     @pets = []
     @species = human
+    @@all << self
   end
   
   def say_species
     puts "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    @@all = []
   end
   
   def pets
